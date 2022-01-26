2d363fab88dc2ae27eec127353f632f9581d0717
omerzam <omerzamsky@gmail.com>
2022-01-26 22:34:52 +0200

    WIP

--- src/ConverterRegistry.ts
+++ src/ConverterRegistry.ts
@@ -11,9 +11,8 @@ import {
   OwnerUpdate as OwnerUpdateEvent,
 } from '../generated/ConverterRegistry/ConverterRegistry'
 
-import { SmartTokenContract } from '../generated/ConverterRegistry/SmartTokenContract'
+import { SmartToken as SmartTokenContract } from '../generated/ConverterRegistry/SmartToken'
 import { LiquidityPoolV1Converter as LiquidityPoolV1ConverterContract } from '../generated/ConverterRegistry/LiquidityPoolV1Converter'
-import { ERC20Token as ERC20TokenContract } from '../generated/ConverterRegistry/ERC20Token'
 
 import {
   ConverterAnchorAdded,
@@ -38,6 +37,10 @@ import {
   LiquidityPoolV1Converter as LiquidityPoolV1ConverterTemplate,
   ERC20Token as ERC20TokenTemplate,
 } from '../generated/templates'
+
+import { getToken } from './helpers/getToken'
+import { getSmartToken } from './helpers/getSmartToken'
+import { getLiquiditypool } from './helpers/getLiquiditypool'
 
 export function handleConverterAnchorAdded(event: ConverterAnchorAddedEvent): void {
   let entity = new ConverterAnchorAdded(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
@@ -79,12 +82,11 @@ export function handleConvertibleTokenRemoved(event: ConvertibleTokenRemovedEven
 
 export function handleSmartTokenAdded(event: SmartTokenAddedEvent): void {
   let smartTokenAddress = event.params._smartToken
-  let smartTokenEntity = SmartToken.load(smartTokenAddress.toHex())
+  let smartTokenObj = getSmartToken(smartTokenAddress)
 
-  if (smartTokenEntity == null) {
-    smartTokenEntity = new SmartToken(smartTokenAddress.toHex())
-
-    SmartTokenTemplate.create(smartTokenAddress)
+  const isNew = smartTokenObj.isNew
+  const smartTokenEntity = smartTokenObj.smartToken
+  if (isNew) {
     log.debug('Smart Token created: {}', [smartTokenAddress.toHex()])
   }
 
@@ -99,14 +101,8 @@ export function handleSmartTokenAdded(event: SmartTokenAddedEvent): void {
 
   let converterAddress = smartTokenContract.owner()
   log.debug('Converter address: {}', [converterAddress.toHex()])
-  let converterEntity = LiquidityPoolV1Converter.load(converterAddress.toHex())
-
-  if (converterEntity == null) {
-    LiquidityPoolV1ConverterTemplate.create(converterAddress)
-    converterEntity = new LiquidityPoolV1Converter(converterAddress.toHex())
-    converterEntity.firstAddedToRegistryBlockNumber = event.block.number
-    converterEntity.firstAddedToRegistryBlockTimestamp = event.block.timestamp
-  }
+  let converterObj = getLiquiditypool(converterAddress)
+  let converterEntity = converterObj.liquidityPoolV1Converter
 
   let converterContract = LiquidityPoolV1ConverterContract.bind(converterAddress)
 
@@ -119,43 +115,8 @@ export function handleSmartTokenAdded(event: SmartTokenAddedEvent): void {
       if (!connectorTokenResult.reverted) {
         let connectorTokenAddress = connectorTokenResult.value
 
-        let connectorTokenEntity = Token.load(connectorTokenAddress.toHex())
-        if (connectorTokenEntity == null) {
-          connectorTokenEntity = new Token(connectorTokenAddress.toHex())
-        }
+        let connectorTokenEntity = getToken(connectorTokenAddress, converterAddress, smartTokenAddress)
 
-        let connectorTokenContract = ERC20TokenContract.bind(connectorTokenAddress)
-
-        let connectorTokenNameResult = connectorTokenContract.try_name()
-        if (!connectorTokenNameResult.reverted) {
-          connectorTokenEntity.name = connectorTokenNameResult.value
-        }
-        let connectorTokenSymbolResult = connectorTokenContract.try_symbol()
-        if (!connectorTokenSymbolResult.reverted) {
-          connectorTokenEntity.symbol = connectorTokenSymbolResult.value
-        }
-        let connectorTokenDecimalsResult = connectorTokenContract.try_decimals()
-        if (!connectorTokenDecimalsResult.reverted) {
-          connectorTokenEntity.decimals = connectorTokenDecimalsResult.value
-        }
-
-        let liquidityPoolTokenEntity = LiquidityPoolToken.load(converterAddress.toHex() + connectorTokenAddress.toHex())
-        if (liquidityPoolTokenEntity === null) {
-          liquidityPoolTokenEntity = new LiquidityPoolToken(converterAddress.toHex() + connectorTokenAddress.toHex())
-        }
-
-        liquidityPoolTokenEntity.token = connectorTokenAddress.toHex()
-        liquidityPoolTokenEntity.liquidityPool = converterAddress.toHex()
-        liquidityPoolTokenEntity.save()
-
-        let tokenSmartTokenEntity = TokenSmartToken.load(connectorTokenAddress.toHex() + smartTokenAddress.toHex())
-        if (tokenSmartTokenEntity === null) {
-          tokenSmartTokenEntity = new TokenSmartToken(connectorTokenAddress.toHex() + smartTokenAddress.toHex())
-        }
-
-        tokenSmartTokenEntity.token = connectorTokenAddress.toHex()
-        tokenSmartTokenEntity.smartToken = smartTokenAddress.toHex()
-        tokenSmartTokenEntity.save()
         // log.debug('Connector Token Converters: {}', [connectorTokenConverters.toString()])
         // connectorTokenEntity.liquidityPools = connectorTokenConverters
         connectorTokenEntity.currentConverterRegistry = event.address.toHex()
@@ -169,52 +130,10 @@ export function handleSmartTokenAdded(event: SmartTokenAddedEvent): void {
     }
   }
 
-  let smartTokenNameResult = smartTokenContract.try_name()
-  if (!smartTokenNameResult.reverted) {
-    smartTokenEntity.name = smartTokenNameResult.value
-  }
-  let smartTokenSymbolResult = smartTokenContract.try_symbol()
-  if (!smartTokenSymbolResult.reverted) {
-    smartTokenEntity.symbol = smartTokenSymbolResult.value
-  }
-  let smartTokenDecimalsResult = smartTokenContract.try_decimals()
-  if (!smartTokenDecimalsResult.reverted) {
-    smartTokenEntity.decimals = smartTokenDecimalsResult.value
-  }
-
   smartTokenEntity.currentConverterRegistry = event.address.toHex()
 
-  // let smartTokenVersionResult = smartTokenContract.try_version()
-  // if (!smartTokenVersionResult.reverted) {
-  //   smartTokenEntity.version = smartTokenVersionResult.value
-  // }
-  // let smartTokenTransfersEnabledResult = smartTokenContract.try_transfersEnabled()
-  // if (!smartTokenTransfersEnabledResult.reverted) {
-  //   smartTokenEntity.transfersEnabled = smartTokenTransfersEnabledResult.value
-  // }
   converterEntity.smartToken = smartTokenAddress.toHex()
   converterEntity.currentConverterRegistry = event.address.toHex()
-  let converterContractRegistryResult = converterContract.try_registry()
-  if (!converterContractRegistryResult.reverted) {
-    converterEntity.currentContractRegistry = converterContractRegistryResult.value.toHex()
-  }
-  // let converterVersionResult = converterContract.try_version()
-  // if (!converterVersionResult.reverted) {
-  //   converterEntity.version = converterVersionResult.value
-  // }
-  let converterOwnerResult = converterContract.try_owner()
-  if (!converterOwnerResult.reverted) {
-    converterEntity.owner = converterOwnerResult.value.toHex()
-  }
-  let converterMaxConversionFeeResult = converterContract.try_maxConversionFee()
-  if (!converterMaxConversionFeeResult.reverted) {
-    converterEntity.maxConversionFee = converterMaxConversionFeeResult.value
-  }
-  let converterTypeResult = converterContract.try_converterType()
-  if (!converterTypeResult.reverted) {
-    converterEntity.type = converterTypeResult.value
-  }
-
   let converterRegistryEntity = ConverterRegistry.load(event.address.toHex())
   if (converterRegistryEntity == null) {
     converterRegistryEntity = new ConverterRegistry(event.address.toHex())
@@ -224,41 +143,10 @@ export function handleSmartTokenAdded(event: SmartTokenAddedEvent): void {
   converterRegistryEntity.lastUsedAtTransactionHash = event.transaction.hash.toHex()
   converterRegistryEntity.lastUsedAtBlockNumber = event.block.number
   converterRegistryEntity.numConverters = converterRegistryEntity.numConverters.plus(BigInt.fromI32(1))
-  // let converterRegistryConverters = converterRegistryEntity.converters || []
-  // if (converterRegistryConverters.length == 0) {
-  //   converterRegistryConverters.push(converterAddress.toHex())
-  // } else if (!converterRegistryConverters.includes(converterAddress.toHex())) {
-  //   converterRegistryConverters.push(converterAddress.toHex())
-  // }
-  // log.debug('Converter Registry Converters: {}', [converterRegistryConverters.toString()])
-  // converterRegistryEntity.converters = converterRegistryConverters
-  // let converterRegistrySmartTokens = converterRegistryEntity.smartTokens || []
-  // if (converterRegistrySmartTokens.length == 0) {
-  //   converterRegistrySmartTokens.push(smartTokenAddress.toHex())
-  // } else if (!converterRegistrySmartTokens.includes(smartTokenAddress.toHex())) {
-  //   converterRegistrySmartTokens.push(smartTokenAddress.toHex())
-  // }
-  // log.debug('Converter Registry Smart Tokens: {}', [converterRegistrySmartTokens.toString()])
-  // converterRegistryEntity.smartTokens = converterRegistrySmartTokens
-  // let converterRegistryConnectorTokens = converterRegistryEntity.connectorTokens || []
-  // for (var i = 0; i < converterConnectorTokens.length; i++) {
-  //   if (!converterRegistryConnectorTokens.includes(converterConnectorTokens[i])) {
-  //     converterRegistryConnectorTokens.push(converterConnectorTokens[i])
-  //   }
-  // }
-  // log.debug('Converter Registry Connector Tokens: {}', [converterRegistryConnectorTokens.toString()])
-  // converterRegistryEntity.connectorTokens = converterRegistryConnectorTokens
-  smartTokenEntity.owner = converterAddress
 
   converterRegistryEntity.save()
   smartTokenEntity.save()
   converterEntity.save()
-  // let converterManagerResult = converterContract.try_manager()
-  // if (!converterManagerResult.reverted) {
-  //   converterEntity.manager = converterManagerResult.value.toHex()
-  // }
-
-  // smartTokenEntity.save()
 
   let entity = new SmartTokenAdded(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
   entity._smartToken = event.params._smartToken
@@ -269,6 +157,23 @@ export function handleSmartTokenRemoved(event: SmartTokenRemovedEvent): void {
   let entity = new SmartTokenRemoved(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
   entity._smartToken = event.params._smartToken
   entity.save()
+
+  log.debug('Smart Token removed from registry: {}', [event.params._smartToken.toHex()])
+  let smartTokenEntity = SmartToken.load(event.params._smartToken.toHex())
+  if (smartTokenEntity === null) {
+    log.debug('Smart Token {} does not exist', [event.params._smartToken.toHex()])
+    smartTokenEntity = new SmartToken(event.params._smartToken.toHex())
+  }
+
+  smartTokenEntity.currentConverterRegistry = null
+  smartTokenEntity.save()
+
+  let liquidityPoolV1Converter = LiquidityPoolV1Converter.load(smartTokenEntity.owner.toHex())
+  if (liquidityPoolV1Converter === null) {
+    liquidityPoolV1Converter = new LiquidityPoolV1Converter(smartTokenEntity.owner.toHex())
+  }
+  liquidityPoolV1Converter.smartToken = null
+  liquidityPoolV1Converter.save()
 }
 
 export function handleOwnerUpdate(event: OwnerUpdateEvent): void {
@@ -276,4 +181,12 @@ export function handleOwnerUpdate(event: OwnerUpdateEvent): void {
   entity._prevOwner = event.params._prevOwner
   entity._newOwner = event.params._newOwner
   entity.save()
+
+  let converterRegistryEntity = ConverterRegistry.load(event.address.toHex())
+  if (converterRegistryEntity === null) {
+    converterRegistryEntity = new ConverterRegistry(event.address.toHex())
+  }
+
+  converterRegistryEntity.owner = event.params._newOwner
+  converterRegistryEntity.save()
 }
