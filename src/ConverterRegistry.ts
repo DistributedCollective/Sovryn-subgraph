97082e30868d5ce8bf40a0f415c0438c3e6f946c
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-05-22 21:05:52 +0300

    Handle SmartTokenRemoved in ConverterRegistry

--- src/ConverterRegistry.ts
+++ src/ConverterRegistry.ts
@@ -1,5 +1,5 @@
-import { SmartTokenAdded as SmartTokenAddedEvent } from '../generated/ConverterRegistry/ConverterRegistry'
-import { SmartTokenAdded, LiquidityPool, ConverterRegistry } from '../generated/schema'
+import { SmartTokenAdded as SmartTokenAddedEvent, SmartTokenRemoved as SmartTokenRemovedEvent } from '../generated/ConverterRegistry/ConverterRegistry'
+import { SmartTokenAdded, LiquidityPool, ConverterRegistry, SmartToken } from '../generated/schema'
 import { createAndReturnSmartToken } from './utils/SmartToken'
 import { createAndReturnConverterRegistry } from './utils/ConverterRegistry'
 
@@ -30,4 +30,22 @@ export function handleSmartTokenAdded(event: SmartTokenAddedEvent): void {
   let entity = new SmartTokenAdded(event.transaction.hash.toHex() + '-' + event.logIndex.toString())
   entity._smartToken = event.params._smartToken
   entity.save()
+}
+
+export function handleSmartTokenRemoved(event: SmartTokenRemovedEvent): void {
+  let smartTokenEntity = SmartToken.load(event.params._smartToken.toHexString())
+  if(smartTokenEntity !== null) {
+    let registry = ConverterRegistry.load(event.address.toHexString())
+    if (registry !== null) {
+      registry.numConverters = registry.numConverters -1
+      registry.save()
+    }
+    smartTokenEntity.currentConverterRegistry = null
+    smartTokenEntity.save()
+    let liquidityPoolEntity = LiquidityPool.load(smartTokenEntity.owner)
+    if(liquidityPoolEntity !== null) {
+      liquidityPoolEntity.activated = false
+      liquidityPoolEntity.save()
+    }
+  }
 }
