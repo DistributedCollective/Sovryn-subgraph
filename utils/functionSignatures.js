b965f21716280defb39ce45114095416efd7b824
BetsyBraddock <k.shall0507+mutant@gmail.com>
2022-09-21 17:17:22 +0100

    Changes to function signature script

--- utils/functionSignatures.js
+++ utils/functionSignatures.js
@@ -8,27 +8,40 @@ const Protocol = require('@graphprotocol/graph-cli/src/protocols/')
 const toolbox = require('gluegun/toolbox');
 const { Command } = require('commander');
 const program = new Command();
+const fs = require('fs-extra')
 program.version('0.0.1');
 const keccak256 = require('keccak256')
-
 const protocolInstance = new Protocol('ethereum')
 const ABI = protocolInstance.getABI()
 
-async function test() {
-    const filePath = 'abis/LoanTokenLogicStandard.json'
-    const pathArr = filePath.split('/')
-    const contractName = pathArr[pathArr.length - 1].split('.json')[0]
-    console.log(contractName)
-    await loadAbiFromFile(filePath).then(abi => {
-        console.log(abi.callFunctionSignatures()._tail.array.map(item => `${item.slice(0, item.indexOf('('))} 0x${keccak256(item).toString('hex').slice(0, 8)}`))
-    })
+async function main() {
+    const csvFile = './functionSignatures.csv'
+    fs.unlinkSync(csvFile)
+    fs.createFile(csvFile)
+    fs.readdir('./abisForSignatures', async (err, files) => {
+        files.forEach(async (file) => {
+            console.log(file)
+            const filePath = `abis/${file}`
+            const contractName = file.split('.json')[0]
+            await loadAbiFromFile(filePath).then(abi => {
+                if (abi.callFunctionSignatures()._tail !== undefined) {
+                    abi.callFunctionSignatures()._tail.array.forEach(item => {
+                        const functionName = item.slice(0, item.indexOf('('))
+                        const signature = `0x${keccak256(item).toString('hex').slice(0, 8)}`
+                        fs.appendFile(csvFile, `${contractName},${functionName},${signature}\n`)
+                    }
+                    )
+                }
+            })
+        });
+    });
 }
 
 
 const loadAbiFromFile = async filename => {
     let exists = await toolbox.filesystem.exists(filename)
     if (!exists) {
-        throw Error('File does not exist.')
+        throw Error(`File ${filename} does not exist.`)
     } else if (exists === 'dir') {
         throw Error('Path points to a directory, not a file.')
     } else if (exists === 'other') {
@@ -38,4 +51,4 @@ const loadAbiFromFile = async filename => {
     }
 }
 
-test()
+main()
