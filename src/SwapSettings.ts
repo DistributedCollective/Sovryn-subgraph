b1ff4a5fa7d1204e3cf21974f14d8ee29b676565
Victor Creed <victor@sovryn.app>
2023-11-09 13:15:48 +0200

    fix: fix swapsetting entity

--- src/SwapSettings.ts
+++ src/SwapSettings.ts
@@ -1,11 +1,11 @@
-import { SwapSettings } from '../generated/schema'
+import { SwapSetting } from '../generated/schema'
 import { ProtocolFeeUpdate } from '../generated/SwapSettings/SwapSettings'
 import { dataSource } from '@graphprotocol/graph-ts'
 
 export function handleProtocolFeeUpdate(event: ProtocolFeeUpdate): void {
-  let settings = SwapSettings.load(dataSource.address().toHexString())
+  let settings = SwapSetting.load(dataSource.address().toHexString())
   if (settings == null) {
-    settings = new SwapSettings(dataSource.address().toHexString())
+    settings = new SwapSetting(dataSource.address().toHexString())
   }
 
   settings.protocolFee = event.params._newProtocolFee.toI32()
